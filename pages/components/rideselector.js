23df419ba85080c1e7800c6cc0783b2c76473e1d
Glory Angelina <angelinavemuri@gmail.com>
2022-06-06 18:37:35 +0530

    next1

--- pages/components/rideselector.js
+++ pages/components/rideselector.js
@@ -4,17 +4,6 @@ import tw from "tailwind-styled-components"
 import { Amblist } from '../data/Amblist'
 
 const Rideselector = ({pickupcoordinates,dropoffcoordinates}) => {
-  const [rideduration,setrideduration]=useState(0)
-
-  useEffect(()=>{
-      rideduration=fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${pickupcoordinates[0]},${pickupcoordinates[1]};${dropoffcoordinates[0]},${dropoffcoordinates[1]}?access_token=pk.eyJ1IjoiZ2xvcnlhbmdlbGluYS0xOSIsImEiOiJjbDN3djU1aWEwZHNvM2pxdDcxb2loY2tpIn0.FyJO6hn93hwqX2CGkmwk5g`
-      ).then(res=>res.json())
-      .then(data=>{
-        setrideduration(data.routes[0].duration/100);
-      })
-      .catch((e)=>console.log(e));
-
-  },[pickupcoordinates,dropoffcoordinates])
 
 
   return (
@@ -28,7 +17,7 @@ const Rideselector = ({pickupcoordinates,dropoffcoordinates}) => {
               <ServiceSection>{car.service}</ServiceSection>
               <TimeSection>5 min away</TimeSection>
           </AmbDetails>
-          <Price>{'Rs'+(rideduration*car.multiplier).toFixed(2)}</Price>
+          <Price>{'Rs'+'0.0'}</Price>
        </Amb>   
          ))}
       </AmbulanceList>
