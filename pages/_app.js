84a91c79a4497fb8997ad4fe9cbb9b19af3737a3
Glory Angelina <angelinavemuri@gmail.com>
2022-06-11 00:26:21 +0530

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
+import { Router } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Router basename="/periwinkle">
+  < Component {...pageProps} />
+    </Router>
+  )
 }
 
 export default MyApp
